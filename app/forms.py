aff3bd5889079fdf9263ee094e2c8c08e7c9a761
Kadaine <kadaine.williams@mymona.uwi.edu>
2018-02-25 16:31:22 +0000

    Lab4 finish

--- app/forms.py
+++ app/forms.py
@@ -2,5 +2,5 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField,FileAllowed,FileRequired
 
 class UploadForm(FlaskForm):
-    upload = FileField('image', validators=[FileRequired(),FileAllowed(['jpg','png','jpeg'], 'Only jpg,jpeg and png images can be uploaded!')])
+    upload = FileField('images', validators=[FileRequired(),FileAllowed(['jpg','png','jpeg'], 'Only jpg,jpeg and png images can be uploaded!')])
     
